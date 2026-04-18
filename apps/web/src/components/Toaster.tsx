1edf094437f25d5b9898f51a6089d0fed839dba2
Sofa Radkova <sr549@duke.edu>
2026-04-18 12:08:34 -0400

    fix

--- apps/web/src/components/Toaster.tsx
+++ apps/web/src/components/Toaster.tsx
@@ -45,7 +45,7 @@ export function Toaster({ toasts, onDismiss }: ToasterProps) {
   if (toasts.length === 0) return null;
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex w-80 flex-col gap-2">
+    <div className="fixed bottom-4 left-4 right-4 z-50 flex flex-col gap-2 sm:left-auto sm:w-80">
       {toasts.map((t) => (
         <ToastItem key={t.id} toast={t} onDismiss={onDismiss} />
       ))}
